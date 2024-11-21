1d8f99ad0fc29290ef2de5825b76df65daf4d259
JEONGMUN LEE <lot9616@gmail.com>
2024-11-21 23:49:27 +0900

    [Update] User Repository save 메서드 추가

--- user/domain/repository/user_repo.py
+++ user/domain/repository/user_repo.py
@@ -1,5 +1,8 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
+from user.domain.user import User
 
 
 class IUserRepository(metaclass=ABCMeta):
-    pass
+    @abstractmethod
+    def save(self, user: User):
+        raise NotImplementedError  # 인터페이스 함수의 구현부는 NotImplementedError를 일으켜서 구현이 필요함을 기술한다.
