fbda1a6abc64b70661c2c912ff5d92af581e78af
JEONGMUN LEE <lot9616@gmail.com>
2024-11-24 17:19:48 +0900

    [Update] 400 Error Handler 추가

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
 
 from user.interface.controllers.user_controller import router as user_routers
 
 app = FastAPI()
 app.include_router(router=user_routers)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=400,
+        content=exc.errors(),
+    )
