eda8432cfbe897c305d77dfb5ee5961bdcfef9ee
JEONGMUN LEE <lot9616@gmail.com>
2024-11-24 17:10:43 +0900

    [Create] CreateUser Pydantic 모델 추가

--- user/interface/controllers/user_controller.py
+++ user/interface/controllers/user_controller.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/users")
 
 
+class CreateUserBody(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
 @router.post("", status_code=201)
-def create_user():
-    return "User Created"
+def create_user(user: CreateUserBody):
+    return user
