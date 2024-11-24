0c4c1a0493c7301ba14a3c1a4f48ae4408b633a0
JEONGMUN LEE <lot9616@gmail.com>
2024-11-24 17:03:24 +0900

    [Add] User Router

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 
+from user.interface.controllers.user_controller import router as user_routers
+
 app = FastAPI()
-
-
-@app.get("/")
-def hello():
-    return {"Hi"}
+app.include_router(router=user_routers)
---
+++ user/interface/controllers/user_controller.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/users")
+
+
+@router.post("", status_code=201)
+def create_user():
+    return "User Created"
