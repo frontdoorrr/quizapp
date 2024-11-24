f56d5b05f0d3981253d066659a254c81dad5159b
JEONGMUN LEE <lot9616@gmail.com>
2024-11-24 16:54:09 +0900

    [Feat] Find user by email

--- user/domain/repository/user_repo.py
+++ user/domain/repository/user_repo.py
@@ -6,3 +6,11 @@ class IUserRepository(metaclass=ABCMeta):
     @abstractmethod
     def save(self, user: User):
         raise NotImplementedError  # 인터페이스 함수의 구현부는 NotImplementedError를 일으켜서 구현이 필요함을 기술한다.
+
+    @abstractmethod
+    def find_by_email(self, email: str) -> User:
+        """
+        Search User by Email.
+        If there is not user, 422 Error occurs.
+        """
+        raise NotImplementedError
