d40f1b660a466d7942afa77f9bbd4d27fdf2c9bc
JEONGMUN LEE <lot9616@gmail.com>
2024-12-22 17:30:11 +0900

    [Update] answer 모델 마이그레이션

--- database_models.py
+++ database_models.py
@@ -1,2 +1,3 @@
 import user.infra.db_models.user
 import game.infra.db_models.game
+import answer.infra.db_models.answer
---
+++ models.py
@@ -0,0 +1,5 @@
+from user.infra.db_models.user import User
+from game.infra.db_models.game import Game
+from answer.infra.db_models.answer import Answer
+
+__all__ = ["User", "Game", "Answer"]
