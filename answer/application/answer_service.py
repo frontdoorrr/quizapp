4b5c49dff1666a9268db0f140535f37d4e7a9891
JEONGMUN LEE <lot9616@gmail.com>
2024-12-30 20:33:57 +0900

    [Add] UserPoint 추가

--- answer/application/answer_service.py
+++ answer/application/answer_service.py
@@ -6,13 +6,20 @@ from dependency_injector.wiring import inject
 from answer.domain.answer import Answer
 from answer.domain.repository.answer_repo import IAnswerRepository
 from game.domain.repository.game_repo import IGameRepository
+from user.domain.repository.user_repo import IUserRepository
 
 
 class AnswerService:
     @inject
-    def __init__(self, answer_repo: IAnswerRepository, game_repo: IGameRepository):
+    def __init__(
+        self,
+        answer_repo: IAnswerRepository,
+        game_repo: IGameRepository,
+        user_repo: IUserRepository,
+    ):
         self.answer_repo = answer_repo
         self.game_repo = game_repo
+        self.user_repo = user_repo
         self.ulid = ULID()
 
     def submit_answer(self, game_id: str, user_id: str, answer_text: str) -> Answer:
@@ -21,10 +28,10 @@ class AnswerService:
 
         # 정답 여부 확인
         is_correct = game.answer.strip().lower() == answer_text.strip().lower()
-        
+
         # 포인트 계산 (임시로 정답이면 10점)
-        point = 10 if is_correct else 0
-        
+        # point = 10 if is_correct else 0
+
         now = datetime.now()
         answer = Answer(
             id=self.ulid.generate(),
@@ -37,7 +44,10 @@ class AnswerService:
             updated_at=now,
             point=point,
         )
-        
+
+        user = self.user_repo.find_by_id(user_id)
+        user.coin
+
         return self.answer_repo.save(answer)
 
     def get_answer(self, id: str) -> Answer:
