4e1eee10906df9c8a11c4f04cf4ec5ed104c5d58
JEONGMUN LEE <lot9616@gmail.com>
2025-02-18 22:29:24 +0900

    [Fix] db sesssion 관리 방식 변경

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
+from typing import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 from config import get_settings
 
@@ -12,3 +13,11 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
